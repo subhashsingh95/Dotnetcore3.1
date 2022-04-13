d8fefc81d2a8a22b034966e76e2bd3c66125785d
subhashsingh95 <96482098+subhashsingh95@users.noreply.github.com>
2022-04-13 23:16:29 +0530

    Update Ubuntu.sh

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -12,9 +12,9 @@ rm -rf /var/www/dotnetubuntu1.com
 mkdir -p /var/www/dotnetubuntu1.com
 cp -R * /var/www/dotnetubuntu1.com
 chmod -R 775 /var/www/dotnetubuntu1.com
-cp -r /var/lib/jenkins/workspace/dotnetubuntu1/UbuntuScript/dotnetubuntu1.conf  /etc/apache2/sites-enabled/
-cp -r /var/lib/jenkins/workspace/dotnetubuntu1/UbuntuScript/dotnetubuntu1.service /etc/systemd/system/
+cp -r /var/lib/jenkins/workspace/dotnetUbuntu1/UbuntuScript/dotnetubuntu1.conf  /etc/apache2/sites-enabled/
+cp -r /var/lib/jenkins/workspace/dotnetUbuntu1/UbuntuScript/subhash.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl start dotnetubuntu1.service
+sudo systemctl start subhash.service
 
 
