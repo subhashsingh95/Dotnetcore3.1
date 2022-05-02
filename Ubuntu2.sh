2908622108409103c7080e18a3dcc67fae314be7
subhashsingh95 <96482098+subhashsingh95@users.noreply.github.com>
2022-05-02 21:53:42 +0530

    Update Ubuntu2.sh

--- Ubuntu2.sh
+++ Ubuntu2.sh
@@ -1,7 +1,11 @@
-dotnet publish -c release
-cd /var/lib/jenkins/workspace/dotnetUbuntu1.1/bin/Release/netcoreapp3.1/publish/
-rm -rf /var/www/dotnetUbuntu1.1.com
-mkdir -p /var/www/dotnetUbuntu1.1.com
+dotnet publish -c release 
+
+cd /var/lib/jenkins/workspace/dotnetUbuntu1.1/bin/Release/netcoreapp3.1/publish/ 
+
+rm -rf /var/www/dotnetUbuntu1.1.com 
+
+mkdir -p /var/www/dotnetUbuntu1.1.com 
+
 cp -R * /var/www/dotnetUbuntu1.1.com
 chmod -R 775 /var/www/dotnetUbuntu1.1.com
 cp -r /var/lib/jenkins/workspace/dotnetUbuntu1.1/UbuntuScript1.1/dotnetUbuntu1.1.conf  /etc/apache2/sites-enabled/
